b10c7b3504b9402927b8789ff1ee3320a071e598
Moisés García <79472169+MoisesGJ@users.noreply.github.com>
2024-11-26 13:17:51 -0600

    feat: Agregar controlador de registro y ruta de API para el registro de usuarios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//controllers
+use App\Http\Controllers\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
